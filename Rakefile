1e01a31b67fffb6713e064dc101921b565c9c479
github username <github email address>
2019-06-12 12:37:26 -0500

    adds session helper methods to application controller

--- Rakefile
+++ Rakefile
@@ -4,6 +4,7 @@ require_relative './config/environment'
 require 'sinatra/activerecord/rake'
 
 
+desc "Pry Console"
 task :console do
   Pry.start
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 User.create(username: "testuser", email: "test@test.com", password: "12345")
 Game.create(name: "Mario")
-consoles = ["Xbox", "PS4", "Switch", "MacOS", "PC"]
+consoles = ["Xbox", "PlayStation 4", "Nintendo Switch", "Wii U", "MacOS", "PC", "iPhone", "Android"]
 consoles.each {|c| Console.create(name: c)}
